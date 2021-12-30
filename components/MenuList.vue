521738b4fa9edd26122bc4943063e6de2a3d7efc
jasenmichael <jasen@jasenmichael.com>
2021-12-29 21:24:59 -0600

    chore: create side panel components

---
+++ components/MenuList.vue
@@ -0,0 +1,122 @@
+<template>
+  <ul>
+    <li
+      v-for="section in menu"
+      v-show="section.slug !== 'README'"
+      :key="section.slug"
+      class="items-center text-lg"
+      :class="
+        section.depth === 0
+          ? ''
+          : section.depth === 1
+          ? 'pl-5 border-l-2 border-slate-700'
+          : section.depth === 2
+          ? 'pl-4'
+          : section.depth === 3
+          ? 'pl-6'
+          : section.depth === 4
+          ? 'pl-8'
+          : section.depth === 5
+          ? 'pl-10'
+          : ''
+      "
+    >
+      <icon
+        v-if="section.type === 'dir'"
+        :ref="
+          'toggle' +
+          (section.path
+            ? section.path.replace(/\//g, '')
+            : section.dir.replace(/\//g, ''))
+        "
+        icon="chevron-right"
+        class="cursor-pointer w-[16px] mx-1 transform transition ease-in-out duration-300"
+        :class="section.depth === 0 && 'rotate-90'"
+        @click="toggleDropdown(section)"
+      />
+      <!-- <span v-if="section.type === 'file'">-</span> -->
+      <nuxt-link
+        :to="
+          section.type === 'dir'
+            ? section.dir
+            : `${section.dir}#${section.slug}`
+        "
+        @click.native="openDropdown(section)"
+      >
+        {{ slugToTitle(section.title || section.slug) }}
+      </nuxt-link>
+      <div
+        v-if="section.children"
+        :ref="
+          section.path
+            ? section.path.replace(/\//g, '')
+            : section.dir.replace(/\//g, '')
+        "
+        class="transform transition duration-700"
+        :class="section.depth !== 0 && 'hidden h-0'"
+      >
+        <MenuList :menu="section.children" />
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'MenuList',
+  props: {
+    menu: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+  data: () => {
+    return {}
+  },
+  computed: {},
+  methods: {
+    toggleDropdown(section) {
+      if (section.type === 'dir') {
+        const refList = section.path
+          ? section.path.replace(/\//g, '')
+          : section.dir.replace(/\//g, '')
+        const refToggle = 'toggle' + refList
+        this.$refs[refList][0].classList.toggle('hidden')
+        this.$refs[refList][0].classList.toggle('h-0')
+        this.$refs[refToggle][0].classList.toggle('rotate-90')
+      }
+    },
+    openDropdown(section) {
+      if (section.type === 'dir') {
+        const refList = section.path
+          ? section.path.replace(/\//g, '')
+          : section.dir.replace(/\//g, '')
+        const refToggle = 'toggle' + refList
+        this.$refs[refList][0].classList.remove('hidden')
+        this.$refs[refList][0].classList.remove('h-0')
+        this.$refs[refToggle][0].classList.add('rotate-90')
+      }
+      this.$store.dispatch('setPanelOpen', false)
+    },
+    slugToTitle(slug) {
+      return this.$utils.slugToTitle(slug)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.nuxt-link-active,
+.nuxt-link-exact-active {
+  @apply text-zinc-500;
+}
+
+.nuxt-link-exact-active {
+  @apply border-slate-500;
+}
+
+svg ~ .nuxt-link-exact-active {
+  @apply rotate-90;
+}
+</style>
---
+++ components/SidePanel.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="flex">
+    <!-- mobile panel bg -->
+    <div
+      class="fixed lg:relative bg-slate-800 h-panel lg:!min-h-0 z-40 w-full transform transition-colors ease-in-out duration-300"
+      :class="
+        panelOpen ? 'w-full  bg-opacity-40' : 'bg-opacity-0 -translate-x-full'
+      "
+      @click="$store.dispatch('setPanelOpen', !panelOpen)"
+    ></div>
+    <aside
+      class="overflow-scroll scrollbar-hide h-panel fixed bg-slate-800 z-40 text-zinc-100 lg:flex flex-col py-10 transition transform ease-in-out duration-300 mb-12"
+      :class="!panelOpen ? '-translate-x-full lg:-translate-x-0' : ''"
+    >
+      <div>
+        <!-- panel menu heading -->
+        <p class="mx-8 font-bold border-b-2 border-zinc-400">Sections</p>
+        <!-- panel menu links -->
+        <MenuList :menu="getMenu" class="w-[400px] px-8" />
+        <!-- panel menu footer -->
+        <div class="bg-inherit mt-10 mx-8 h-full border-t-2 border-zinc-400">
+          <p>nav-footer</p>
+        </div>
+      </div>
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SidePanel',
+  props: {
+    // panelOpen: {
+    //   type: Boolean,
+    //   required: true,
+    //   default: () => false,
+    // },
+    scrollPos: {
+      type: Number,
+      required: true,
+      default: () => 0,
+    },
+  },
+  data: () => {
+    return {
+      menu: [],
+    }
+  },
+  computed: {
+    getMenu() {
+      return this.$store.getters.getMenu
+    },
+    panelOpen() {
+      return this.$store.getters.getPanelOpen
+    },
+  },
+}
+</script>
