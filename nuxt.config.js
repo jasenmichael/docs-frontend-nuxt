4ed3ea7bd1440454d826883a97aab02d5e08e32e
jasenmichael <jasen@jasenmichael.com>
2021-12-29 21:24:12 -0600

    chore: edit config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,7 @@
+import copyContentImages from './plugins/copyContentImages'
+
+copyContentImages('content', 'static', ['.md'])
+
 export default {
   head: {
     title: 'DOCS',
@@ -10,17 +14,22 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   css: ['@/assets/css/main.css'],
-  plugins: [],
+  plugins: ['~/plugins/utils'],
   components: true,
   buildModules: [
     '@nuxtjs/eslint-module',
     '@nuxtjs/stylelint-module',
     '@nuxt/postcss8',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/fontawesome',
   ],
-  modules: ['@nuxtjs/axios', '@nuxtjs/pwa', '@nuxt/content', '@nuxtjs/strapi'],
-  axios: {
-    baseURL: '/',
-  },
+  modules: [
+    // '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    '@nuxt/content',
+    '@nuxtjs/strapi',
+  ],
+  // axios: {},
   pwa: {
     manifest: {
       lang: 'en',
@@ -32,6 +41,47 @@ export default {
       process.env.NODE_ENV === 'development'
         ? 'http://localhost:1337/api'
         : process.env.STRAPI_URL,
+  },
+  googleFonts: {
+    display: 'swap',
+    prefetch: true,
+    preconnect: true,
+    preload: true,
+    families: {
+      Roboto: [100, 200, 300, 400],
+      'Source+Code+Pro': {
+        wght: [200, 300, 400],
+        ital: [300],
+      },
+      // 'Open+Sans': true,
+      Montserrat: true,
+      // Lato: [100, 300],
+      Raleway: [100, 200, 400],
+    },
+  },
+  fontawesome: {
+    icons: {
+      solid: ['faCog', 'faChevronRight', 'faChevronDown', 'faSignOutAlt'],
+    },
+    component: 'icon',
+    // component: 'FaIcon',
+    // suffix: false
+    // suffix: {
+    //   suffix: false
+    // }
+  },
+  publicRuntimeConfig: () => {
+    return {
+      dev:
+        process.env.NODE_ENV === 'development'
+          ? {
+              auth: {
+                identifier: process.env.DEV_IDENTIFIER || '',
+                password: process.env.DEV_PASSWORD || '',
+              },
+            }
+          : null,
+    }
   },
   build: {
     postcss: {
