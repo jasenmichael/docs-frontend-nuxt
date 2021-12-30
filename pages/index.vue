3933d8d2eb04543bc50db3d66ccbc8a7be109e54
jasenmichael <jasen@jasenmichael.com>
2021-12-29 21:23:28 -0600

    chore: add index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,30 @@
 <template>
-  <div>
-    <h1 class="text-3xl">DOCS</h1>
-    <button @click="logout()">log out</button>
+  <div class="light py-6">
+    <!-- <pre class="overflow-scroll mr-auto dark px-4 min-w-full">{{
+      $store.state.menu
+    }}</pre> -->
+    <nuxt-content
+      :document="page"
+      class="prose-lg prose-headings:my-4 prose-li:my-1 min-w-full"
+    />
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  layout: 'DefaultLayout',
   middleware: 'auth',
-  methods: {
-    logout() {
-      this.$strapi.logout()
-      this.$router.push('/login')
-    },
+  async asyncData({ $content, error }) {
+    const page = await $content('README')
+      .fetch()
+      .catch(() => {
+        error({ statusCode: 404, message: 'Page not found' })
+      })
+    return { page }
   },
+  // data: () => {
+  //   return {}
+  // },
 }
 </script>
